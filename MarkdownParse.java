24d8b8cf3877f5e80b7d6043bca99e53147cee3c
AliceFeather <zhz083@ucsd.edu>
2022-05-08 19:52:44 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -33,6 +33,8 @@ public class MarkdownParse {
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
+        System.out.println("hello");
+        
         
     }
 }
